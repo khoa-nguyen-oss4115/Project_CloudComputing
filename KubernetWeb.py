8f03bdab0a79eb0d05d3d72106bb509aee8f1c9e
NguyenQuangHung911 <hungquang0938@gmail.com>
2021-05-29 10:10:55 +0700

    Cập nhật cho Web các hàm xử lý cho system của Ubuntu

--- KubernetWeb.py
+++ KubernetWeb.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, redirect, url_for, request
-import os
+import subprocess
 
 app = Flask(__name__)
 
 def Xuly():
+    list_files = subprocess.run(["helm ls"])
+    print("The exit code was: %d" % list_files.returncode)
     return
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    
+    Xuly()
     if request.method == "POST":
         return render_template("index.html")
     else:
